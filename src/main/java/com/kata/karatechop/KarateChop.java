f3537aaa59f9d3916d7c83f1807c3cc18f816873
oswald.s <dev.oswald.s@gmail.com>
2024-06-22 14:25:32 +0200

    Karate Chop : Refactoring.

--- src/main/java/com/kata/karatechop/KarateChop.java
+++ src/main/java/com/kata/karatechop/KarateChop.java
@@ -3,29 +3,33 @@ package com.kata.karatechop;
 import java.util.List;
 
 public class KarateChop {
-    private KarateChop(){}
+    private KarateChop() {
+    }
 
-    public static int chop(int i, List<Integer> ints) {
+    public static int chop(int target, List<Integer> searchInput) {
 
-        if (ints.isEmpty()) {
+        if (searchInput.isEmpty()) {
             return -1;
         }
 
-        int mid = ints.size() / 2;
+        int middleIndex = searchInput.size() / 2;
 
-        if (i == ints.get(mid)) {
-            return mid;
+        if (target == searchInput.get(middleIndex)) {
+            return middleIndex;
         }
 
-        if (i > ints.get(mid) && ints.size() > 1) {
-            List<Integer> subList = ints.subList(mid, ints.size());
-            int chop = chop(i, subList);
-            return chop == -1 ? -1 : chop + mid;
-        }
+        if (searchInput.size() > 1) {
 
-        if (i < ints.get(mid) && ints.size() > 1) {
-            List<Integer> subList = ints.subList(0, mid);
-            return chop(i, subList);
+            if (target > searchInput.get(middleIndex)) {
+                List<Integer> inputGreaterHalf = searchInput.subList(middleIndex, searchInput.size());
+                int chop = chop(target, inputGreaterHalf);
+                return chop == -1 ? -1 : chop + middleIndex;
+            }
+
+            if (target < searchInput.get(middleIndex)) {
+                List<Integer> inputSmallerHalf = searchInput.subList(0, middleIndex);
+                return chop(target, inputSmallerHalf);
+            }
         }
         return -1;
     }
