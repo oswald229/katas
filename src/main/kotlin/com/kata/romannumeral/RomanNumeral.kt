077468d712a8bf012197d9c0cc17772a31466b55
oswald.s <dev.oswald.s@gmail.com>
2024-10-15 22:42:42 +0200

    Roman Numeral : Done.

---
+++ src/main/kotlin/com/kata/romannumeral/RomanNumeral.kt
@@ -0,0 +1,56 @@
+package com.kata.romannumeral
+
+import java.util.*
+import kotlin.math.abs
+
+class RomanNumeral {
+    private val romanNumeralDefinition: TreeMap<Int, String> = TreeMap(
+        mapOf(
+            Pair(1, "I"),
+            Pair(5, "V"),
+            Pair(10, "X"),
+            Pair(50, "L"),
+            Pair(100, "C"),
+            Pair(500, "D"),
+            Pair(1000, "M"),
+            Pair(9999999, "!!!!!")
+        )
+    )
+
+    fun toRoman(number: Int): String? {
+        if (number == 0) {
+            return ""
+        }
+        if (number == 1) {
+            return romanNumeralDefinition[number]
+        }
+
+        val closestInferiorRomanNumeralIdx = getClosestInferiorRomanNumeralIdx(number)
+
+        if (closestInferiorRomanNumeralIdx + 1 < romanNumeralDefinition.size) {
+            val nextSuperiorRomanNumeral = romanNumeralKeys()[closestInferiorRomanNumeralIdx + 1]
+
+            val deltaWithNextSuperiorRomanNumeral = abs(number - nextSuperiorRomanNumeral)
+            if (isRomanNumeralDefinition(deltaWithNextSuperiorRomanNumeral)
+                &&
+                deltaWithNextSuperiorRomanNumeral != number
+            ) {
+                return romanNumeralDefinition[deltaWithNextSuperiorRomanNumeral] + romanNumeralDefinition[nextSuperiorRomanNumeral]
+            }
+        }
+
+        val nextRomanNumeral = romanNumeralKeys()[closestInferiorRomanNumeralIdx]
+        return romanNumeralDefinition[nextRomanNumeral] + toRoman(number - nextRomanNumeral)
+    }
+
+    private fun getClosestInferiorRomanNumeralIdx(number: Int): Int {
+        return romanNumeralDefinition
+            .filter { entry -> number % entry.key != number }
+            .toList().size - 1
+    }
+
+    private fun romanNumeralKeys() = romanNumeralDefinition.keys.toList()
+
+    private fun isRomanNumeralDefinition(number: Int) = romanNumeralDefinition.containsKey(number)
+
+}
