f249e7db909f087ae22fa230d10bda750be02e99
oswald <dev.oswald.s@gmail.com>
2025-09-06 10:07:40 +0200

    Fix: Kotlin/Java conflict.

--- src/test/kotlin/com/kata/romannumeral/RomanNumeralKotTest.kt
+++ src/test/kotlin/com/kata/romannumeral/RomanNumeralKotTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
-class RomanNumeralTest {
+class RomanNumeralKotTest {
 
     /*
 
